d4d37844264e8a6fdf6e2d494b9e27ae91f65fe9
chahyasantoso <chahya.santoso@gmail.com>
2024-11-19 18:11:10 +0700

    test sync

--- index.js
+++ index.js
@@ -109,5 +109,5 @@ app.post('/api/persons', (request, response) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`New Sync Server running on port ${PORT}`)
 })
